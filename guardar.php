595945e111c045ba1443836002462254e5a4b4a5
Leohayequipo <lic.mannanice@gmail.com>
2025-06-17 18:41:00 -0300

    Guardar.php
    Guarda todo en la base

---
+++ guardar.php
@@ -0,0 +1,22 @@
+<?php
+$conexion = new mysqli("localhost", "root", "", "proyecto_basico");
+
+if ($conexion->connect_error) {
+    die("Error de conexión: " . $conexion->connect_error);
+}
+
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$email = $_POST['email'];
+$telefono = $_POST['telefono'];
+$fecha = $_POST['fecha_nacimiento'];
+
+$sql = "INSERT INTO personas (nombre, apellido, email, telefono, fecha_nacimiento)
+        VALUES ('$nombre', '$apellido', '$email', '$telefono', '$fecha')";
+
+if ($conexion->query($sql) === TRUE) {
+    header("Location: listar.php");
+} else {
+    echo "Error: " . $conexion->error;
+}
+?>
